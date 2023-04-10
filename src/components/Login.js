e954e709b66585dd19324dbd43091f7609f01ba8
Tim <voncurleone@gmail.com>
2023-04-10 19:14:11 -0400

    Initial commit.

---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import "../stylesheets/Login.css"
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you can handle the login logic with the username and password
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className={"login-form"}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={handleUsernameChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;
